28a560118f526ae035ec4c5cb7fed5134f99dd80
corecodinglab <krahul31091@gmail.com>
2026-03-02 19:05:45 +0530

    Create add-two-numbers.c

---
+++ Function-Programs/add-two-numbers.c
@@ -0,0 +1,21 @@
+/*
+Program: Add Two Numbers Using Function
+Description: Demonstrates function usage in C.
+*/
+
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Sum = %d\n", add(x, y));
+
+    return 0;
+}
